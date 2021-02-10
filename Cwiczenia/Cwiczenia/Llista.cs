dbc67a12f4b8327b8e97182f6aa033f430acbb6b
llisowski6706 <67202830+llisowski6706@users.noreply.github.com>
2021-02-10 23:19:27 +0100

    Add files via upload

---
+++ Cwiczenia/Cwiczenia/Llista.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Cwiczenia
+{
+    internal class Llista<T> : IEnumerable<T>, IEnumerable
+    {
+        private T[] array = new T[100];
+        private int lenght = 0;
+
+        public void Add(T args)
+        {
+            array[lenght++] = args;
+        }
+        public void Remove(T args)
+        {
+            bool found = false;
+            int index = 0;
+            foreach (T item in array)
+            {
+                if (!found && item.Equals(args))
+                {
+                    found = true;
+                }
+                if (found && index + 1 < lenght)
+                {
+                    array[index] = array[index + 1];
+                }
+                index++;
+            }
+            lenght--;
+        }
+        public int Count()
+        {
+            return lenght;
+        }
+        public void AddRange(T[] args)
+        {
+            foreach (T item in args)
+            {
+                Add(item);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+}
---
+++ Cwiczenia/Cwiczenia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cwiczenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Llista<int> pesel = new Llista<int>();
+            int[] test = { 1, 2, 3 };
+            pesel.Add(5);
+            pesel.Add(1111111);
+            //   pesel.Remove(5);
+            a = pesel.Count();
+            foreach (int item in pesel)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(a);
+            pesel.AddRange(test);
+            Console.WriteLine("------------------");
+            foreach (int item in pesel)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+    
